c95c28c0dd69ac94a0f48d4621abe1615843bcb7
Ralkey <Ralkey@outlook.com>
2024-08-30 12:32:00 +0200

    Fixed fitgirl_repacks.py

--- fitgirl_repacks.py
+++ fitgirl_repacks.py
@@ -1,10 +1,24 @@
-#VERSION: 1.0
-# AUTHORS: Bioux
+#VERSION: 1.1
+# AUTHORS: Bioux, Ralkey
 
 import json
+import logging
+import os
+from http.cookiejar import CookieJar
 from urllib.parse import unquote
-from helpers import retrieve_url
-from novaprinter import prettyPrinter
+from urllib.request import HTTPCookieProcessor, build_opener
+
+try:
+    from novaprinter import prettyPrinter  # Production environment
+except ImportError:
+    # Mock for development
+    def prettyPrinter(data):
+        print(data)
+
+
+USER_AGENT: tuple = ('User-Agent',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
+    'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15')
 
 
 class fitgirl_repacks(object):
@@ -12,25 +26,34 @@ class fitgirl_repacks(object):
     name = 'FitGirl Repacks'
     supported_categories = {'all': ''}
 
+    cj = CookieJar()
+    session = build_opener(HTTPCookieProcessor(cj))
+    session.addheaders = [USER_AGENT]
+
     def search(self, what, cat='all'):
         search_url = 'https://hydralinks.cloud/sources/fitgirl.json'
 
-        response = retrieve_url(search_url)
-        response_json = json.loads(response)
+        with self.session.open(search_url, timeout = 10) as response:
+            response = response.read()
+            response_json = json.loads(response)
 
-        what = unquote(what)
-        search_terms = what.lower().split()
+            what = unquote(what)
+            search_terms = what.lower().split()
 
-        for result in response_json['downloads']:
-            if any(term in result['title'].lower() for term in search_terms):
-                res = {'link': self.download_link(result),
-                       'name': result['title'],
-                       'size': result['fileSize'],
-                       'seeds': '-1',
-                       'leech': '-1',
-                       'engine_url': self.url,
-                       'desc_link': '-1'}
-                prettyPrinter(res)
+            for result in response_json['downloads']:
+                if any(term in result['title'].lower() for term in search_terms):
+                    res = {'link': self.download_link(result),
+                        'name': result['title'],
+                        'size': result['fileSize'],
+                        'seeds': '-1',
+                        'leech': '-1',
+                        'engine_url': self.url,
+                        'desc_link': '-1'}
+                    prettyPrinter(res)
 
     def download_link(self, result):
             return result['uris'][0]
+    
+if __name__ == "__main__":
+    test = fitgirl_repacks()
+    test.search("grand theft auto")
